dec7fe5ebf0455ce712c06895bfcbdedd333927d
Elharam4 <55320959+elostora@users.noreply.github.com>
2020-11-01 16:36:58 +0200

    Create ELHARAM4_whtsapp.py

---
+++ ELHARAM4_whtsapp.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear")
+print("Loading........... .")
+print ("HACK WHATSAPP BY ELHARAM4")
+from random import choices
+elharam4 = input ("inter your phone numper to hack : ")
+list=["123467","765490","979878","010963","264790","101009","389118","144340","285927","112628","730695","432262","708626","981828","514744","896633","837014","436655","303061","287738","191836"]
+x=choices(list,k=5)
+print(x)
